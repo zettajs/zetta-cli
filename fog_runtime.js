edf80e292742c272600ed7a74234372713f323c1
Adam Magaluk <AdamMagaluK@gmail.com>
2014-02-21 16:11:34 -0500

    Broke out registry into seperate file

--- fog_runtime.js
+++ fog_runtime.js
@@ -1,67 +1,9 @@
-var fs = require('fs');
-var path = require('path');
-
 var FogAppLoader = require('./fog_app_loader');
 var Scientist = require('./scientist');
 var DevicesResource = require('./api_resources/devices');
 var EventEmitter = require('events').EventEmitter;
 var util = require('util');
-
-
-function Registry(){
-  this.devices = [];
-  this.json_devices = [];
-  this.path = path.join(process.cwd(),'registry.json');
-};
-
-Registry.prototype.load = function(cb) {
-  var self = this;
-
-  fs.readFile(this.path,function(err,buf){    
-    if(err)
-      return cb(err);
-
-    try{
-      var data = JSON.parse(buf.toString());
-
-      if(data.devices)
-        self.json_devices = data.devices;
-      cb();
-    }catch(err){
-      cb(err);
-    }
-  });
-};
-
-Registry.prototype.save = function(cb) {
-  var devices = this.devices.map(function(device){
-    return {
-      type : device.type,
-      name : device.name,
-      data : device.data
-    };
-  });
-
-  var data = JSON.stringify({devices : devices});
-  fs.writeFile(this.path, data,cb);
-};
-
-Registry.prototype.add = function(machine,cb) {
-  this.devices.push(machine);
-  this.save(cb);
-};
-
-Registry.prototype.setupDevice = function() {
-  var machine = Scientist.configure.apply(null,arguments);
-  this.devices.push(machine);
-};
-
-
-
-
-
-
-
+var Registry = require('./registry');
 
 
 var FogRuntime = module.exports = function(argo, scouts) {
