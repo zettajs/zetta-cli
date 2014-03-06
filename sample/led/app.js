843fe61e53e6c500c23e84e767b95ecbf539c26c
Kevin Swiber <kswiber@gmail.com>
2014-03-06 10:37:44 -0500

    Pointing bootstrapper to localhost, adding SPDY module, adding LED/photosensor example

---
+++ sample/led/app.js
@@ -0,0 +1,35 @@
+var HelloApp = module.exports = function() {
+  this.name = 'hello';
+};
+
+HelloApp.prototype.init = function(elroy) {
+  elroy.get('joes-office-photosensor', function(err, photosensor) {
+    elroy.get('joes-office-led', function(err, led) {
+      led.on('turn-on', function() {
+        console.log('turning on');
+      });
+
+      led.on('turn-off', function() {
+        console.log('turning off');
+      });
+
+      photosensor.on('change', function(value) {
+        if (value < 100) {
+          led.call('turn-on');
+        } else {
+          led.call('turn-off');
+        }
+      });
+
+      led.on('error', function(err) {
+        if (typeof err !== app.TransitionError) {
+          throw err;
+        }
+      });
+
+      elroy.expose(led);
+      elroy.expose(photosensor);
+    });
+  });
+};
+
