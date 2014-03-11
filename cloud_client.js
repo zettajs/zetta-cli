abf8cfd560189e58a9cc3cae40f64f41efe06c24
Kevin Swiber <kswiber@gmail.com>
2014-03-11 10:20:26 -0400

    Removing the need for FakeSocket.

--- cloud_client.js
+++ cloud_client.js
@@ -10,9 +10,15 @@ module.exports = function(argo, url, cb) {
   var app = argo
     .use(function(handle) {
       handle('request', function(env, next) {
+        console.log('in request handler');
         var id = env.request.headers['elroy-message-id'];
         env.response.setHeader('elroy-message-id', id);
 
+        next(env);
+      });
+      handle('response', function(env, next) {
+        //console.log('source buffer:', fake.source.buffer);
+        //console.log('source buffer length:', fake.source.buffer.length);
         next(env);
       });
     })
@@ -24,25 +30,31 @@ module.exports = function(argo, url, cb) {
     ssl: false
   }, app.run);
 
+  server.on('stream', function(socket) {
+    console.log('on stream!');
+  });
+
   ws.on('open', function() {
     pubsub.setSocket(ws);
+    //server.emit('connection', fake);
+    server.emit('connection', ws._socket);
   });
 
   ws.on('error', function(e) {
     console.error(e.stack);
   });
 
-  var fake = new FakeSocket();
+  /*var fake = new FakeSocket();
 
   fake.onwrite = function(data) {
+    console.log('fake.onwrite');
     ws.send(data, { binary: true });
-  };
+  };*/
 
   ws.on('message', function(data) {
-    fake.source.write(data);
+    console.log('on message');
+    //fake.source.write(data);
   });
-  
-  server.emit('connection', fake);
   
   cb(server);
 };
